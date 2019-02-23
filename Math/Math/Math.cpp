a9ad43e17782a1ee035b6f44bb053cf334ddada0
haseebuchiha <haseeb9009@gmail.com>
2019-02-23 13:15:53 +0500

    Implement your changes on these files lads. Go wild

---
+++ Math/Math/Math.cpp
@@ -0,0 +1,31 @@
+#include "Math.h"
+
+void Math::Display(int num)
+{
+	cout << num << endl;
+}
+
+int Math::Multiply(int a, int b)
+{
+	return 0;
+}
+
+int Math::Add(int a, int b)
+{
+	return 0;
+}
+
+int Math::Subtract(int a, int b)
+{
+	return 0;
+}
+
+float Math::Divide(int a, int b)
+{
+	return 0.0f;
+}
+
+int main()
+{
+
+}
---
+++ Math/Math/Math.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+class Math
+{
+public:
+	void Display(int num);
+	int Multiply(int a, int b);
+	int Add(int a, int b);
+	int Subtract(int a, int b);
+	float Divide(int a, int b);
+};
